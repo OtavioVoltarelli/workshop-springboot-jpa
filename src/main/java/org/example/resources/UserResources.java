18b5a6e0c89a11309f6768894c638e0d18ee9e27
Otavio Voltarelli <otavio.voltarelli@hotmail.com>
2024-07-08 16:50:50 -0300

    User entity and resource

---
+++ src/main/java/org/example/resources/UserResources.java
@@ -0,0 +1,26 @@
+package org.example.resources;
+
+
+import org.example.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Otávio Voltarelli", "otavio@gmail.com", "99999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+
+
+
+
+
+
+
+}
