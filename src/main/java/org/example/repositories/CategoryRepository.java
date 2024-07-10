a413309f189e64293925c8c304ae65de693e81fa
Otavio Voltarelli <otavio.voltarelli@hotmail.com>
2024-07-10 15:18:32 -0300

    Category

--- src/main/java/org/example/repositories/CategoryRepository.java
+++ src/main/java/org/example/repositories/CategoryRepository.java
@@ -1,7 +1,8 @@
 package org.example.repositories;
 
+import org.example.entities.Category;
 import org.example.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
--- src/main/java/org/example/resources/CategoryResource.java
+++ src/main/java/org/example/resources/CategoryResource.java
@@ -1,8 +1,8 @@
 package org.example.resources;
 
 
-import org.example.entities.Order;
-import org.example.services.OrderService;
+import org.example.entities.Category;
+import org.example.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,21 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/orders")
-public class OrderResources {
+@RequestMapping(value = "/categorys")
+public class CategoryResource {
 
     @Autowired
-    private OrderService service;
+    private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<Order>> findAll() {
-        List<Order> list = service.findAll();
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Order> findById(@PathVariable Long id) {
-        Order obj = service.findById(id);
+    public ResponseEntity<Category> findById(@PathVariable Long id) {
+        Category obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
