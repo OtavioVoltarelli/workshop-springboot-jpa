df76426cc585c4a6b30d10b917559b1b43f2c103
Otavio Voltarelli <otavio.voltarelli@hotmail.com>
2024-07-10 14:13:49 -0300

    Order Resource/Service/Repositories

---
+++ src/main/java/org/example/repositories/OrderRepository.java
@@ -0,0 +1,8 @@
+package org.example.repositories;
+
+import org.example.entities.Order;
+import org.example.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
