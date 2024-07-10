8fb8166d94e1f751d4ff45e68b25fa72a4c47571
Otavio Voltarelli <otavio.voltarelli@hotmail.com>
2024-07-10 16:26:30 -0300

    ManyToMany association with JoinTable

--- src/main/java/org/example/entities/Category.java
+++ src/main/java/org/example/entities/Category.java
@@ -1,5 +1,6 @@
 package org.example.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -17,7 +18,8 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
@@ -44,7 +46,6 @@ public class Category implements Serializable {
         this.name = name;
     }
 
-    @Transient
     public Set<Product> getProducts() {
         return products;
     }
